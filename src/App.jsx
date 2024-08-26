ef5b090313dff6f2bda78e5cad10a2010a32fe27
tstidwill <tara.stidwill@gmail.com>
2024-08-26 16:25:33 -0400

    Setup global mixins including breakpoint and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import "./App.scss";
 
 function App() {
   return (
     <>
-      <p>Hello! Welcome to Tara Stidwill's portfolio</p>
+      <h1>H1</h1>
+      <h2>h2</h2>
+      <h3>TARA STIDWILL</h3>
+      <h4>this is h4</h4>
+      <h5>header5</h5>
+      <p>para</p>
+      <h6>Footer</h6>
     </>
   );
 }
